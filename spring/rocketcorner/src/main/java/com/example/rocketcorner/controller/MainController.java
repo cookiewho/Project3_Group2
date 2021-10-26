dcc50581f79ca322257330129342e4fbdc224671
Robert Huskins <robertchuskins@gmail.com>
2021-10-26 14:14:00 -0700

    docker no longer runs in root mode

--- spring/rocketcorner/src/main/java/com/example/rocketcorner/controller/MainController.java
+++ spring/rocketcorner/src/main/java/com/example/rocketcorner/controller/MainController.java
@@ -3,8 +3,9 @@ package com.example.rocketcorner.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MainController {
 
     @GetMapping("/")
