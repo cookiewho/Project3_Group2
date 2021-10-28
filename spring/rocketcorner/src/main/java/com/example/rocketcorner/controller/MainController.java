7846d86512fd648b5ac2b885411d8d19d032e8ef
Robert Huskins <robertchuskins@gmail.com>
2021-10-28 12:17:27 -0700

    figure out how to upload docker image to heroku

--- spring/rocketcorner/src/main/java/com/example/rocketcorner/controller/MainController.java
+++ spring/rocketcorner/src/main/java/com/example/rocketcorner/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
     @GetMapping("/")
     public ResponseEntity<?> helloWorld() {
-        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+        return new ResponseEntity<>("Hello everyone", HttpStatus.OK);
     }
 
 }
