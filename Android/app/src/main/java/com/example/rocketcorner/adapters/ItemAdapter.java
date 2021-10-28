e53fc176d5ed8251adf0e8fb59e3b3946f4e39de
Jamiel Capatayan <26446581+JamielCapatayan@users.noreply.github.com>
2021-10-28 11:42:14 -0700

    included androidX. Working Build

--- Android/app/src/main/java/com/example/rocketcorner/adapters/ItemAdapter.java
+++ Android/app/src/main/java/com/example/rocketcorner/adapters/ItemAdapter.java
@@ -1,16 +1,47 @@
 package com.example.rocketcorner.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rocketcorner.R;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    Context context;
+    // TODO Create list of Items class & add to constructor
 
     private String[] localDataSet;
+
+    public ItemAdapter(Context context){
+        this.context = context;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.shop_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet[position]);
+    }
 
     /**
      * Provide a reference to the type of views that you are using
@@ -18,11 +49,13 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView itemName;
+        RelativeLayout container;
+        // TODO May need to use glide to load images
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-
+            container = itemView.findViewById(R.id.container);
             itemName = (TextView) view.findViewById(R.id.itemName);
         }
 
@@ -41,24 +74,6 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
         localDataSet = dataSet;
     }
 
-    // Create new views (invoked by the layout manager)
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new view, which defines the UI of the list item
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.shop_item, viewGroup, false);
-
-        return new ViewHolder(view);
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
-        viewHolder.getTextView().setText(localDataSet[position]);
-    }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
