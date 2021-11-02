d1b5f2483ccaaf446a7d2cb5f4fe1c1193be42b0
amemariee <awall@csumb.edu>
2021-11-01 21:58:19 -0700

    Added check if feilds are entered and email is a valid email

--- Android/app/src/main/java/com/example/rocketcorner/RegisterUser.java
+++ Android/app/src/main/java/com/example/rocketcorner/RegisterUser.java
@@ -1,13 +1,86 @@
 package com.example.rocketcorner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class RegisterUser extends AppCompatActivity implements  View.OnClickListener{
+
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.fullName);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.registerUser:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+//        Checks if fields are entered and valid
+        if(fullName.isEmpty()){
+            editTextFullName.setError("Name is required");
+            editTextFullName.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPassword.setError("Password must be more than 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
     }
 }
