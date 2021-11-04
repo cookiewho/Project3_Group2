f8765f6620e750d3690f209d4738793095f4f373
Robert Huskins <robertchuskins@gmail.com>
2021-11-04 10:48:45 -0700

    first attempt uploading to heroku

--- spring/rocketcorner/src/main/java/com/example/rocketcorner/RocketcornerApplication.java
+++ spring/rocketcorner/src/main/java/com/example/rocketcorner/RocketcornerApplication.java
@@ -9,6 +9,9 @@ public class RocketcornerApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(RocketcornerApplication.class, args);
+
+		System.out.println("\n\n\nRob look here\n\n\n");
+		System.out.println(System.getenv("TEST"));
 	}
 
 }
