bacf7714cc27574061e614c45217d9cd9f7dd112
David Gaeta <dinkwho@gmail.com>
2021-11-30 10:53:54 -0800

    Illegal Ik, but adding email to admin object Just in case we needed, also updated firebase admin user to match admin object

--- spring/rocketcorner/src/main/java/com/example/rocketcorner/objects/Admin.java
+++ spring/rocketcorner/src/main/java/com/example/rocketcorner/objects/Admin.java
@@ -3,6 +3,7 @@ package com.example.rocketcorner.objects;
 public class Admin {
     public static final String ADMIN_ID = "1";
     public static final String ADMIN_USERNAME = "ADMIN";
+    public static final String ADMIN_EMAIL = "admin@admin.com";
     public static final String ADMIN_PASSWORD = "ADMIN";
 
 }
